9792951cba62cf09c2e0c6f4082fe151a7253821
Karthikeya Nagandla <karthikeyanagandla7@gmail.com>
2022-04-24 22:23:13 +0530

    Lab-2

--- set-2/q-4.js
+++
@@ -1,6 +0,0 @@
-angular.module("myApp", []).controller("personCtrl", function ($scope) { 
-    ($scope.collegeName = ""),
-    ($scope.Name = function () { 
-        return $scope.collegeName;
-    });
-});
