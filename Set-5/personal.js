f645773b89779f38ebb33be95d1592d320a8aea3
Karthikeya Nagandla <karthikeyanagandla7@gmail.com>
2022-10-27 17:27:42 +0530

    Delete unused

--- Set-5/personal.js
+++
@@ -1,8 +0,0 @@
-angular.module("myApp", []).controller("personCtrl", function ($scope) {
-    ($scope.firstName = "Karthikeya"),
-        ($scope.lastName = "Nagandla"),
-        ($scope.getName = function () {
-            return $scope.firstName + " " + $scope.lastName;
-        });
-});
-
